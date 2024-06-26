46cd8fe5d475a1db1f7bb342dfa57f3ecf683b78
Rahul Chavda <rahulchavda6000@gmail.com>
2024-06-26 20:51:40 +0530

    #

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,17 +6,17 @@ export default function Footer() {
   return (
     <section className="f-section">
         
-            <div className="f-title">
-            <a href="/" className='f-text'><span className='title-f'>Dev</span>Rahul</a><span className='footer-t'>.</span>
-            </div>
-            <div className="f-copyright">
-              <p className='copy-text'>Copyright &copy; 2024 Dev<span> Rahul </span>.</p>
-            </div>
-            <div className="footer-social">
-              <div className='footer-social-icon'> <FaInstagram /></div>
-              <div className='footer-social-icon f-color'> <FaGithub /></div>
-              <div className='footer-social-icon'> <FaTwitter /></div>
-            </div>
+      <div className="f-title">
+        <a href="/" className='f-text'><span className='title-f'>Dev</span>Rahul</a><span className='footer-t'>.</span>
+      </div>
+      <div className="f-copyright">
+        <p className='copy-text'>Copyright &copy; 2024 Dev<span> Rahul </span>.</p>
+      </div>
+      <div className="footer-social">
+        <div className='footer-social-icon'> <FaInstagram /></div>
+        <div className='footer-social-icon f-color'> <FaGithub /></div>
+        <div className='footer-social-icon'> <FaTwitter /></div>
+      </div>
        
     </section>
   )
