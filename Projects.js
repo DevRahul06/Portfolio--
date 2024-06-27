61ce5c0c73bd559b45dba3be8ad15596eebc8620
Rahul Chavda <rahulchavda6000@gmail.com>
2024-06-27 17:36:06 +0530

    #

--- Projects.js
+++ Projects.js
@@ -1,7 +1,6 @@
 import note from "./src/assets/Project-image/Notekeeper.jpg"
 import Photography from "./src/assets/Project-image/Photography-website.jpg"
-import { IoLogoJavascript } from "react-icons/io5";
-import {FaHtml5,FaCss3} from "react-icons/fa"
+import food from "./src/assets/Project-image/Food-website.png"
 
 const projects = [
     {
@@ -19,7 +18,15 @@ const projects = [
         stack: ['HTML', 'CSS', 'JavaScript', ],
         sourceCode:'https://github.com/DevRahul06/Photography_Website',
         livePreview:'https://devrahul06.github.io/Photography_Website/'
-    }
+    },
+    {
+        name:"Agency-website",
+        description:"A simple Agency-website",
+        stack: ['HTML', 'CSS', 'JavaScript', ],
+        img: food,
+        sourceCode:'https://github.com/DevRahul06/Agency-website',
+        livePreview:'https://devrahul06.github.io/Notekeeper/'
+    },
 ];
 
 export {projects};
